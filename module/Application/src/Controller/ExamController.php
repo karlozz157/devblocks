a81d10d733ce146a4e3333d7fe09f07d0a43b7e5
Carlos Romero <karlozz157@gmail.com>
2016-11-30 16:48:11 -0600

    Added detail for examn and changed gitignore

--- module/Application/src/Controller/ExamController.php
+++ module/Application/src/Controller/ExamController.php
@@ -4,5 +4,14 @@ namespace Application\Controller;
 
 class ExamController extends CrudController
 {
+    /**
+     * @return array
+     */
+    public function detailAction()
+    {
+        $id     = $this->params()->fromRoute('id');
+        $detail = $this->getService('manager')->findById($this->entityClass, $id);
 
+        return ['detail' => $detail];
+    }
 }
